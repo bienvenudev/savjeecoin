508b09609b12563fe9401cc72ef9286172e54072
bienvenudev <cbienvenu007@gmail.com>
2026-02-28 23:43:27 +0200

    feat: add input validation for transaction creation

--- savjeecoin-frontend-react/src/pages/CreateTransaction.tsx
+++ savjeecoin-frontend-react/src/pages/CreateTransaction.tsx
@@ -12,8 +12,6 @@ export function CreateTransaction({ onTxCreated }) {
   const walletKey = blockchainService.walletKeys[0].publicKey;
 
   function addTransaction() {
-    if (toAddress === '' || amount === '') throw Error('Transaction must include from and to address') // is this a good way to handle? or i should have added validation on the inputs?
-
     const newTx = new Transaction(walletKey, toAddress, Number(amount));
     newTx.signTransaction(blockchainService.walletKeys[0].keyObj)
 
@@ -35,10 +33,10 @@ export function CreateTransaction({ onTxCreated }) {
       <input disabled type='text' id='from-address' className='w-full border border-gray-400 rounded p-2 mb-8' value={walletKey} />
 
       <label htmlFor="to-address">To Address</label>
-      <input id='to-address' className='w-full border border-gray-400 rounded p-2 mb-8' value={toAddress} onChange={(e) => { setToaddress(e.target.value) }} />
+      <input required id='to-address' className='w-full border border-gray-400 rounded p-2 mb-8' value={toAddress} onChange={(e) => { setToaddress(e.target.value) }} />
 
       <label htmlFor="amount">Amount</label>
-      <input type="number" id='amount' className='w-full border border-gray-400 rounded p-2 mb-8' value={amount} onChange={(e) => { setAmount(e.target.value) }} />
+      <input required min="0.01" type="number" id='amount' className='w-full border border-gray-400 rounded p-2 mb-8' value={amount} onChange={(e) => { setAmount(e.target.value) }} />
 
       <button className='mt-8 font-bold outline-2 bg-blue-500 text-white cursor-pointer p-3 rounded hover:bg-blue-600 active:bg-blue-700' onClick={addTransaction}>Sign & create transaction</button>
     </>
